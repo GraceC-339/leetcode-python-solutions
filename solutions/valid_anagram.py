d0153e7d919fb4eee2da3ddf7c715a3adcb1b0ec
Grace C <99157913+GraceC-339@users.noreply.github.com>
2026-02-09 16:01:49 +0800

    update solution

--- solutions/valid_anagram.py
+++ solutions/valid_anagram.py
@@ -3,14 +3,12 @@ class Solution:
         if len(s) != len(t):
             return False
         
-        freq_s = {}
-        freq_t = {}
-        
-        for char in s:
-            freq_s[char] = freq_s.get(char,0) + 1
-        
-        for char in t:
-            freq_t[char] = freq_t.get(char,0) + 1
+        freq_s, freq_t = {}, {}
+
+        for i in range(len(s)):
+            freq_s[s[i]] = freq_s.get(freq_s[i],0) + 1
+            freq_t[t[i]] = freq_t.get(freq_t[i],0) + 1
+            
         
         return freq_s == freq_t
 
